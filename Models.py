a1ddda3ca3dfd7f0e7f751b1a9830faf8dfbd541
rotemgoren <34604067+rotemgoren@users.noreply.github.com>
2020-04-10 18:16:24 +0300

    Add files via upload

---
+++ Models.py
@@ -0,0 +1,149 @@
+import torch
+import torchvision
+from torchsummary import summary
+from torch import nn
+import torch.utils.data as data
+import numpy as np
+
+from torchsummary import summary
+import torch.nn.functional as F
+
+
+NUM_SLICES=16
+class Dataset(data.Dataset):
+    def __init__(self, x,y,step):
+
+
+
+        
+            self.x = torch.cat([torch.FloatTensor(x[:,:,i:i+NUM_SLICES]).unsqueeze(0).unsqueeze(1) for i in range(0,int(x.shape[2]/NUM_SLICES)*NUM_SLICES-step,step)])
+            self.y = torch.cat([torch.FloatTensor(y[:,:,i:i+NUM_SLICES]).unsqueeze(0).unsqueeze(1) for i in range(0,int(x.shape[2]/NUM_SLICES)*NUM_SLICES-step,step)])
+
+
+    def __getitem__(self, index):
+        return self.x[index,:,:,:,:], self.y[index,:,:,:,:]
+
+    def __len__(self):
+        return self.x.size(0)
+
+
+class DownConvBlock(nn.Module):
+    def __init__(self,in_channels, mid_channels,out_channels,kernel_size=3,num_layers=1,device='cpu'):
+        super(DownConvBlock, self).__init__()
+        self.device=device
+        self.num_layers=num_layers
+        self.conv1=nn.Conv3d(in_channels=in_channels,out_channels=mid_channels,padding=1,kernel_size=kernel_size,stride=1).to(self.device).float()
+        self.conv2 = []
+        for _ in range(1,self.num_layers):
+            self.conv2.append(nn.Conv3d(in_channels=mid_channels,out_channels=mid_channels,padding=1,kernel_size=kernel_size,stride=1).to(self.device).float())
+        self.conv3 = nn.Conv3d(in_channels=mid_channels, out_channels=out_channels,padding=1, kernel_size=kernel_size, stride=1).to(self.device).float()
+
+        self.pool4 = nn.MaxPool3d(kernel_size=2,stride=2)
+        self.bn1 = nn.BatchNorm3d(mid_channels).to(self.device).float()
+        self.bn2 = nn.BatchNorm3d(out_channels).to(self.device).float()
+
+        self.prelu=nn.ReLU().to(self.device).float()
+
+    def forward(self,x):
+        x1=self.conv1(x)
+        x1=self.bn1(x1)
+        x1=self.prelu(x1)
+
+        x2=x1
+        for i in range(1,self.num_layers):
+            x2 = self.conv2[i-1](x2)
+            x2 = self.bn1(x2)
+            x2 = self.prelu(x2)
+
+        del x1
+        torch.cuda.empty_cache()
+        x3 = x2 + x
+        x4 = self.conv3(x3)
+        x4 = self.bn2(x4)
+        x4 = self.prelu(x4)
+        x4 = self.pool4(x4)
+
+        return x3,x4
+
+class UpConvBlock(nn.Module):
+    def __init__(self,in_channels, mid_channels, out_channels,kernel_size=3,num_layers=1,device='cpu'):
+        super(UpConvBlock, self).__init__()
+        self.num_layers=num_layers
+        self.device=device
+        self.conv1=nn.Conv3d(in_channels,mid_channels,padding=1,kernel_size=kernel_size,stride=1).to(self.device).float()
+        self.conv2 = []
+        for _ in range(1,self.num_layers):
+            self.conv2.append(nn.Conv3d(mid_channels,mid_channels,padding=1,kernel_size=kernel_size,stride=1).to(self.device).float())
+        self.conv3 = nn.ConvTranspose3d(mid_channels, out_channels, kernel_size=2, stride=2).to(self.device).float()
+        self.bn1 = nn.BatchNorm3d(mid_channels).to(self.device).float()
+        self.bn2 = nn.BatchNorm3d(out_channels).to(self.device).float()
+        self.prelu = nn.ReLU().to(self.device).float()
+
+    def forward(self,x):
+        x1=self.conv1(x)
+        x1=self.bn1(x1)
+        x1=self.prelu(x1)
+
+        x2=x1
+        for i in range(1,self.num_layers):
+            x2 = self.conv2[i-1](x2)
+            x2 = self.bn1(x2)
+            x2 = self.prelu(x2)
+
+        x3 = x2 + x
+        x4 = self.conv3(x3)
+        x4 = self.bn2(x4)
+        x4 = self.prelu(x4)
+
+        return x4
+
+
+
+class VNet(nn.Module):
+    def __init__(self,device):
+        super(VNet,self).__init__()
+        self.down_conv1 = DownConvBlock(in_channels=1,mid_channels=16,out_channels=32,num_layers=1,device=device)
+        self.down_conv2 = DownConvBlock(in_channels=32,mid_channels=32, out_channels=64, num_layers=3,device=device)
+        self.down_conv3 = DownConvBlock(in_channels=64,mid_channels=64, out_channels=128, num_layers=3,device=device)
+        self.down_conv4 = DownConvBlock(in_channels=128,mid_channels=128, out_channels=256, num_layers=3,device=device)
+
+        self.up_conv5 = UpConvBlock(in_channels=256,mid_channels=256, out_channels=128, num_layers=3,device=device)
+        self.up_conv6 = UpConvBlock(in_channels=128+128,mid_channels=128+128, out_channels=64, num_layers=3,device=device)
+        self.up_conv7 = UpConvBlock(in_channels=64+64,mid_channels=64+64, out_channels=32, num_layers=3,device=device)
+        self.up_conv8 = UpConvBlock(in_channels=32 + 32, mid_channels=32 + 32, out_channels=32, num_layers=3,device=device)
+
+        self.conv9=nn.Conv3d(in_channels=32+16,out_channels=1,padding=1,kernel_size=3,stride=1).to(device).float()
+        self.sigmoid = nn.Sigmoid()
+
+
+    def forward(self,x):
+        x1,x = self.down_conv1(x)
+        x3,x = self.down_conv2(x)
+        x5,x = self.down_conv3(x)
+        x7,x = self.down_conv4(x)
+
+
+        x = self.up_conv5(x)
+        x = torch.cat((x7,x),dim=1)
+
+        x= self.up_conv6(x)
+        x = torch.cat((x5,x),dim=1)
+
+        x = self.up_conv7(x)
+        x = torch.cat((x3,x),dim=1)
+
+        x = self.up_conv8(x)
+        x = torch.cat((x1, x), dim=1)
+
+        x= self.conv9(x)
+        x = self.sigmoid(x)
+
+        return x
+
+
+#model = UNetWithMobileNet().cuda()
+#summary(model,input_size=(3,224,224))
+#print(model)
+
+#inp = torch.rand((2, 3, 512, 512)).cuda()
+#out = model(inp)
---
+++ lungs.py
@@ -0,0 +1,371 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 21:06:36 2017
+
+@author: Rotem Goren
+"""
+
+import os #do directory operations
+import pandas as pd #noce for data analysis
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import math
+#import tensorflow as tf
+import glob
+import gzip
+import nibabel as nib
+from Models import VNet,Dataset
+import torch
+from torch import nn
+from collections import OrderedDict
+from tqdm import tqdm
+import time
+
+#from mask_functions import mask2rle,rle2mask
+
+
+IMG_PX_SIZE =512
+
+
+data_dir='D:\\Task06_Lung\\Task06_Lung'
+#labels_df=pd.read_csv('D:/siim-acr-pneumothorax-segmentation/train-rle-sample.csv',names=['Id','Values'],index_col=False)
+#labels_df.head()
+
+def chunks(l,n):
+    for i in range(0,len(l),n):
+        yield l[i:i+n]
+
+
+
+
+def preprocess_data(patient,IMG_PX_SIZE=50,visulize=False):
+    #label=labels_df[labels_df['Id']==patient[:-4]]['Values'].values[0]
+
+    img = nib.load(imagesTr_path+patient)
+    img_data=img.get_fdata()
+    #img_data = img_data[:, :, int(img_data.shape[2]/2) - int(SLICES / 2):int(img_data.shape[2]/2) + int(SLICES / 2)]
+
+
+    img_data = cv2.resize(img_data, (IMG_PX_SIZE, IMG_PX_SIZE))
+
+    img_data = (img_data-np.min(img_data,axis=(0,1)))/(np.max(img_data,axis=(0,1))-np.min(img_data,axis=(0,1)))
+    img_data=np.array(img_data,dtype=np.float32)
+
+    img = nib.load(labelsTr_path+patient)
+    label_data=img.get_fdata()
+    #label_data = label_data[:, :, int(label_data.shape[2]/2) - int(SLICES / 2):int(label_data.shape[2]/2) + int(SLICES / 2)]
+
+    label_data = cv2.resize(label_data, (IMG_PX_SIZE, IMG_PX_SIZE))
+
+    label=[np.zeros_like(label_data),np.zeros_like(label_data)]
+    #label[0][label_data==0] = 1
+    #label[1][label_data!=0] = 1
+    label=label_data
+    label=np.array(label,dtype=np.float32)
+    if(visulize==True):
+        #while(True):
+        #y = fig.add_subplot(4, 4, i+1)
+        for i in range(img_data.shape[2]):
+
+            cv2.imshow('im',np.array(img_data[ :, :, i]*255,dtype=np.uint8))
+            cv2.waitKey(10)
+
+            cv2.imshow('label',np.array(label_data[ :, :, i]*255,dtype=np.uint8))
+            cv2.waitKey(10)
+
+
+    return img_data,label
+    #new_slices = []
+    #slices=[cv2.resize(np.array(each_slice.pixel_array),(IMG_PX_SIZE,IMG_PX_SIZE)) for each_slice in slices]
+
+def DiceLoss(x,y):
+    numerator = 2 * torch.sum(x * y, dim=(2,3,4))
+    denominator = torch.sum(x + y, dim=(2,3,4))
+    #eps = torch.FloatTensor([1e-6]).to(device)
+    return 1 - numerator / denominator
+
+def train_model(x_train,y_train,x_valid,y_valid):
+    torch.manual_seed(0)
+    ngpu=1
+    BATCH_SIZE=1
+    EPOCHS=50
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
+    model_file=data_dir+'\\VNet.pth'
+
+    model = VNet(device)
+    if (os.path.isfile(model_file)):
+        state_dict = torch.load(model_file, map_location=device)
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            if (k[:7] == 'module.'):
+                name = k[7:]  # remove `module.`
+                new_state_dict[name] = v
+            else:
+                new_state_dict[k] = v
+        # load params
+        model.load_state_dict(new_state_dict)
+
+    if (device.type == 'cuda') and (ngpu > 1):
+        model = nn.DataParallel(model, list(range(ngpu)), dim=0)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.5, 0.999))
+
+    for epoch in range(EPOCHS):
+        train_loss = 0
+        valid_loss = 0
+        total_train = 0
+        correct_train = 0
+        total_valid = 0
+        correct_valid = 0
+        model.train()
+        start = time.time()
+        i=0
+        for x,y in tqdm(zip(x_train,y_train)):
+
+            try:
+                dataset = Dataset(x, y,8)
+                dataloader = torch.utils.data.DataLoader(dataset, batch_size=ngpu,
+                                                               shuffle=True, num_workers=1,
+                                                               pin_memory=True)  # divided into batches
+                optimizer.zero_grad()
+                for (X,gt) in (dataloader):
+                    if(gt[:,0,:,:,:].sum()>0):
+                        #start = time.time()
+                        i+=1
+                        X=X.to(device, non_blocking=True).float()
+                        gt=gt.to(device, non_blocking=True).float()
+
+                        o = model(X)
+                        weight=((gt.shape[2]*gt.shape[3]*gt.shape[4]-gt.sum(dim=(2,3,4)))/gt.sum(dim=(2,3,4))).unsqueeze(2).unsqueeze(3).unsqueeze(4)
+                        loss = nn.BCELoss(weight=weight)(o,gt)
+                        #loss = DiceLoss(o,gt)
+
+
+                        optimizer.zero_grad()
+                        loss.backward()
+
+
+                        train_loss += loss.item()
+                        #if (i % int(len(dataloader)/10) == 0):
+                        optimizer.step()
+
+
+
+                        #predicted = torch.argmax(o.data, 1).sum()
+                        #true = torch.argmax(gt.data, 1).sum()
+
+                        predicted = o.data
+                        predicted[predicted < 0.5] = 0
+                        predicted[predicted > 0.5] = 1
+                        true = gt.data
+
+                        total_train+=true.sum()
+                        correct_train+=predicted.sum()
+
+                        visulize=False
+                        if (visulize == True):
+                            # while(True):
+                            # y = fig.add_subplot(4, 4, i+1)
+                            x=X.cpu().numpy()
+                            y=true.cpu().numpy()
+                            predict = predicted.cpu().numpy()
+
+                            for j in range(x.shape[4]):
+
+                                cv2.imshow('im', np.array(x[0,0, :, :, j] * 255, dtype=np.uint8))
+                                cv2.waitKey(10)
+
+                                cv2.imshow('label', np.array(y[0,0, :, :, j] * 255, dtype=np.uint8))
+                                cv2.waitKey(10)
+
+                                cv2.imshow('predict', np.array(predict[0,0, :, :, j] * 255, dtype=np.uint8))
+                                cv2.waitKey(10)
+
+                        #total_train += true.size(0) * true.size(1) * true.size(2)*true.size(3)
+                        #correct_train += predicted.eq(true.data).sum().item()
+                        torch.cuda.empty_cache()
+
+            except:
+                pass
+        train_accuracy = correct_train / total_train
+        train_loss = train_loss / i
+        torch.cuda.empty_cache()
+
+
+        #model.eval()
+        i=0
+        with torch.no_grad():
+            for x, y in tqdm(zip(x_valid, y_valid)):
+                try:
+                    dataset = Dataset(x, y,8)
+                    dataloader = torch.utils.data.DataLoader(dataset, batch_size=ngpu,
+                                                             shuffle=True, num_workers=1,
+                                                             pin_memory=True)  # divided into batches
+
+                    for X, gt in dataloader:
+                        if (gt[:,0, :, :, :].sum() > 0):
+                            i+=1
+                            X = X.to(device).float()
+                            gt = gt.to(device).float()
+
+                            o = model(X)
+                            #loss = nn.BCELoss()(o,gt)
+                            weight = ((gt.shape[2] * gt.shape[3] * gt.shape[4] - gt.sum(dim=(2, 3, 4))) / gt.sum(dim=(2, 3, 4))).unsqueeze(2).unsqueeze(3).unsqueeze(4)
+                            loss = nn.BCELoss(weight=weight)(o, gt)
+                            #loss = DiceLoss(o,gt)
+
+
+                            valid_loss += loss.item()
+
+                            #predicted = torch.argmax(o.data, 1).sum()
+                            #true = torch.argmax(gt.data, 1).sum()
+
+                            predicted = o.data
+                            predicted[predicted < 0.5] = 0
+                            predicted[predicted > 0.5] = 1
+                            true = gt.data
+
+                            total_valid+=true.sum()
+                            correct_valid+=predicted.sum()
+                            #total_valid += true.size(0) * true.size(1) * true.size(2) * true.size(3)
+                            #correct_valid += predicted.eq(true.data).sum().item()
+                            torch.cuda.empty_cache()
+                except:
+                    pass
+            valid_accuracy = correct_valid / total_valid
+            valid_loss = valid_loss / i
+            torch.cuda.empty_cache()
+        print("Est time={} sec".format(time.time() - start))
+
+        print('[{}/{}]\tTrain loss: {}\t Train Acc: {}\tValid loss:{}\t Valid Acc:{}'.format(epoch, EPOCHS, train_loss,
+                                                                                             train_accuracy, valid_loss,
+                                                                                             valid_accuracy))
+        torch.save(model.state_dict(), '%s' % model_file)
+
+def test_model(x_test,y_test,visulize = True):
+    torch.manual_seed(0)
+    ngpu=1
+    BATCH_SIZE=1
+    EPOCHS=50
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
+    model_file=data_dir+'\\VNet.pth'
+
+    model = VNet(device)
+    if (os.path.isfile(model_file)):
+        state_dict = torch.load(model_file, map_location=device)
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            if (k[:7] == 'module.'):
+                name = k[7:]  # remove `module.`
+                new_state_dict[name] = v
+            else:
+                new_state_dict[k] = v
+        # load params
+        model.load_state_dict(new_state_dict)
+
+    if (device.type == 'cuda') and (ngpu > 1):
+        model = nn.DataParallel(model, list(range(ngpu)), dim=0)
+
+    model.train()
+    with torch.no_grad():
+        for x, y in tqdm(zip(x_test, y_test)):
+
+            # try:
+            dataset = Dataset(x, y, 16)
+            dataloader = torch.utils.data.DataLoader(dataset, batch_size=ngpu,
+                                                     shuffle=False, num_workers=1,
+                                                     pin_memory=True)  # divided into batches
+
+            for (X, gt) in (dataloader):
+                #if (gt[:, 0, :, :, :].sum() > 0):
+                    # start = time.time()
+
+                    X = X.to(device, non_blocking=True).float()
+                    gt = gt.to(device, non_blocking=True).float()
+
+                    o = model(X)
+                    #loss = nn.BCELoss()(o, gt)
+
+                    #loss.backward()
+
+                    predicted = o.data
+                    predicted[predicted < 0.5] = 0
+                    predicted[predicted > 0.5] = 1
+                    true = gt.data
+
+
+                    visulize = True
+                    if (visulize == True):
+                        # while(True):
+                        # y = fig.add_subplot(4, 4, i+1)
+                        x = X.cpu().numpy()
+                        y = true.cpu().numpy()
+                        predict = predicted.cpu().numpy()
+
+                        for j in range(x.shape[4]):
+                            cv2.imshow('im', np.array(x[0, 0, :, :, j] * 255, dtype=np.uint8))
+                            cv2.waitKey(10)
+
+                            cv2.imshow('label', np.array(y[0, 0, :, :, j] * 255, dtype=np.uint8))
+                            cv2.waitKey(10)
+
+                            cv2.imshow('predict', np.array(predict[0, 0, :, :, j] * 255, dtype=np.uint8))
+                            cv2.waitKey(10)
+
+                    # total_train += true.size(0) * true.size(1) * true.size(2)*true.size(3)
+                    # correct_train += predicted.eq(true.data).sum().item()
+                    torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+
+    data = []
+    label = []
+
+    imagesTr_path=data_dir+'\\imagesTr\\'
+    labelsTr_path = data_dir + '\\labelsTr\\'
+    if (os.path.isfile(data_dir+'\\train_data.npy')):
+        train_data=np.load(data_dir + '\\train_data.npy')
+        train_label=np.load(data_dir + '\\train_label.npy')
+
+    else:
+        for num, patientFile in enumerate(glob.glob(os.path.join(imagesTr_path, "*.gz"))[0:20]):
+
+            patient = patientFile.split('\\')[-1]
+            if (num % 10 == 0):
+                print(num)
+            try:
+
+                img_data, label_data = preprocess_data(patient, IMG_PX_SIZE, visulize=False)
+
+                data.append(img_data)
+                label.append(label_data)
+
+            except KeyError as e:
+                print('This is unlabeld data')
+
+
+        #np.save(data_dir + '\\train_data.npy',train_data)
+        #np.save(data_dir + '\\train_label.npy', train_label)
+
+
+    vaildRatio=0.8
+    train_data = data[:int(len(data)*vaildRatio)]
+    train_label = label[:int(len(label) * vaildRatio)]
+
+    vaild_data = data[int(len(data)*vaildRatio):]
+    vaild_label = label[int(len(label) * vaildRatio):]
+
+    train_model(train_data,train_label,vaild_data,vaild_label)
+    test_model(train_data,train_label,visulize = True)
+
+    # img_data=torch.FloatTensor(img_data).unsqueeze(0).unsqueeze(1)
+    # model=VNet()
+    # y=model(img_data)
+
+    #np.save('muchdata-{}-{}-{}.npy'.format(IMG_PX_SIZE,IMG_PX_SIZE,HM_SLICES),much_data)
+
+
